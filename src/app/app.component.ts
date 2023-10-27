4ff2efc25e665490cc5ed8a758f831bf5e540ca6
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-10-27 10:13:10 -0300

    ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-lista-de-compras';
+export class AppComponent implements OnInit, DoCheck {
 
-  constructor() { }
+  title = 'app-lista-de-compras';
+  listaDeCompra!: Array<Item>;
+  itemParaSerEditado!: Item;
+
+  constructor(
+    private listaSevice: ListaDeCompraService
+    ) { }
+
+  ngDoCheck(): void {
+    this.listaSevice.atualizarLocalStorage();
+  }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaSevice.getListaDeCompra();
+    console.log(this.listaDeCompra);
+  }
+
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
+
+  deletarItem(id: number){
+    const index = this.listaDeCompra.findIndex((item) => item.id == id)
+    this.listaDeCompra.splice(index,1);
+  }
+
+  limparLista(){
+    this.listaDeCompra = [];
+  }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() itemQueVaiSerEditado!: Item;
+  editando: boolean = false;
+  textoBtn: string = 'Salvar item';
+  valorItem!: string;
+
+  constructor(
+    private listaService: ListaDeCompraService
+  ) { }
+
+  editarItem(){
+    this.listaService.editarItemDaLista(this.itemQueVaiSerEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+   // console.log(changes['itemQueVaiSerEditado'].currentValue);
+    if (!changes['itemQueVaiSerEditado'].firstChange) {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
+  }
 
   ngOnInit(): void { }
+
+  adicionarItem(){
+    //console.log(this.valorItem);
+    this.listaService.adicionarItemNalista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = '';
+  }
 }
